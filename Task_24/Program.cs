fc3994e3e36cdfc9634f1cafc04ef2da9ec26df3
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-24 11:08:27 +0700

    добавила задачи

---
+++ Task_24/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+// for (int i = 0; i <= num; i++)
+// {
+//     // sum = sum + i;
+//     sum += i;
+// }
+// Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
+
+// Реализуем с помощью метода
+
+int SumNumber(int number)
+{
+    int sum = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int result = SumNumber(num);
+Console.WriteLine($"Сумма чисел от 1 до {num} = {result}");
---
+++ Task_26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int NumberDigit(int num)
+{
+    int count = 0;
+    if (num == 0) return 1;
+    while (num > 0)
+    {
+        count++;
+        num = num / 10;
+    }
+    return count;
+}
+int result = NumberDigit(number);
+Console.WriteLine($"Колличество цифр в числе {number}: {result}");
